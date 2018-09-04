e4781bd88f4b2d52f587b9334357cefe7a832279
DESKTOP-S1BQLEO\Tam Do Khac <tamdk@gosol.com.vn>
2018-09-04 17:18:40 +0700

    - thay ảnh default

--- MyhomeBuildForVNU/core/config/images.js
+++ MyhomeBuildForVNU/core/config/images.js
@@ -3,11 +3,13 @@ import noConnection from "../images/no_connection.png";
 import noData from "../images/no_data.png";
 import noHome from "../../assets/no_home.png";
 import locationDissable from "../images/location_err.png";
+import noSchoolInfo from "../../assets/no_school_info.png";
 
 export default {
   noData,
   noConnection,
   error,
   noHome,
-  locationDissable
+  locationDissable,
+  noSchoolInfo
 };
